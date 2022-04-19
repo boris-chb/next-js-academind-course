ddcb58d02730114c6c2dd6014ab580a1c4952ec0
Boris Ciobirca <bciobirca@letsgetchecked.com>
2022-04-19 13:58:48 +0100

    fixed imports

--- components/events/EventItem.jsx
+++ components/events/EventItem.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import Link from "next/link";
-import classes from "./styles/EventItem.module.css";
+import React from 'react';
+import classes from './styles/EventItem.module.css';
 
 // Components
-import Button from "../ui/Button";
+import Button from '../ui/Button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 const EventItem = ({ title, image, date, location, id }) => {
-  const formattedDate = new Date(date).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric"
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
   });
 
-  const formattedAddress = location.replace(",", "\n");
+  const formattedAddress = location.replace(',', '\n');
   const exploreEventLink = `/events/${id}`;
 
   return (
@@ -22,14 +24,21 @@ const EventItem = ({ title, image, date, location, id }) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{formattedDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreEventLink}>Explore Event</Button>
+          <Button link={exploreEventLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
